f0b5ffa1e42d9f431ae00329bb4cfc0b45afd52c
NikolayGrishko <nikolay.grishko.2000@gmail.com>
2024-10-28 16:53:11 +0200

    change :focus to js

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,17 @@
-  
+const hList = document.querySelectorAll('.header-nav__list li a')
+
+hList.forEach(item =>{ 
+    item.addEventListener('click', (e) =>{
+    hList.forEach(el=>{ el.classList.remove('is-active'); });
+    item.classList.add('is-active')
+})
+})
+
+const page = document.querySelectorAll('.table-pagination li a')
+
+page.forEach(item =>{
+    item.addEventListener('click', (e) =>{
+        page.forEach(el=>{ el.classList.remove('show'); });
+        item.classList.add('show')
+    })
+})
