94a863603457468fa90baa7a2f0bd5e13d35b4be
NikolayGrishko <nikolay.grishko.2000@gmail.com>
2024-11-10 22:31:01 +0200

    done. 13h

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -15,3 +15,11 @@ page.forEach(item =>{
         item.classList.add('show')
     })
 })
+
+const hamburger = document.querySelector('.hamburger')
+const headerNav = document.querySelector('.header-nav')
+
+hamburger.addEventListener('click', function (){
+    hamburger.classList.toggle('is-active')
+    headerNav.classList.toggle('show')
+})
